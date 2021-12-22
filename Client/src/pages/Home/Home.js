9ec7e6ed75990a5e950eccc4d8c1125da889b963
Deep Vasan <deepvasan10@gmail.com>
2021-12-23 00:41:47 +0530

    Home component added

---
+++ Client/src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Home.css";
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <div className="typewriter">
+                <h1>Welcome to  <span style={{color: 'red'}}>B1</span> hostel..!!</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
