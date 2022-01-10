6680a2415770372a4b6cd9852049682a7115de50
Deep Vasan <deepvasan10@gmail.com>
2022-01-10 23:06:14 +0530

    added carousel

---
+++ Client/src/components/Carousel/Carousel.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Carousel.css';
+
+const Carousel = () => {
+    return (
+        <div className="carousel-container">
+            <div className="carousel-container-text">
+                Dive deep into our journey filled with fun and memories...
+            </div>
+            <div className="carousel" />
+        </div>
+    )
+}
+
+export default Carousel;
