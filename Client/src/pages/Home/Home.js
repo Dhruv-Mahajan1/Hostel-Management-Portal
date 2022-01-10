521566d3af6acfb97bb0c251745613dcfecec8d4
Deep Vasan <deepvasan10@gmail.com>
2022-01-10 22:58:22 +0530

    refactor

--- Client/src/pages/Home/Home.js
+++ Client/src/pages/Home/Home.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import "./Home.css";
+import AboutUs from '../../components/About Us/AboutUs';
+import Carousel from '../../components/Carousel/Carousel';
 
 const Home = () => {
     return (
         <div className="home-container">
-            <div className="typewriter">
-                <h1>Welcome to  <span style={{color: 'red'}}>B1</span> hostel..!!</h1>
+            <div className="home">
+                <div className="typewriter">
+                    <h1>Welcome to  <span style={{color: 'red'}}>B1</span> hostel..!!</h1>
+                </div>
             </div>
+            <AboutUs />
+            <Carousel />
         </div>
     )
 }
